071139c45c5aba2f1f90c6a740efb63be4a5026e
Felipe Martins <felipe.martins@fortics.com.br>
2020-01-05 17:17:53 -0300

    Filtrando posts ppor categoria

--- teste/my_app/views.py
+++ teste/my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 
@@ -14,13 +14,27 @@ def home_with_param(request, post_id):
 
 
 def post_list(request):
-    posts = Post.objects.all()
+
+    if 'category_id' in request.GET:
+        category_id = request.GET['category_id']
+        category = Category.objects.get(id=category_id)
+        posts = Post.objects.filter(categories=category)
+    else:
+        posts = Post.objects.all()
+
+    categories = Category.objects.all()
     data = {
-        'posts': posts
+        'posts': posts,
+        'categories': categories
     }
     return render(request, 'posts_list.html', data)
 
 
 def post_show(request, post_id):
     post = Post.objects.get(id=post_id)
-    return render(request, 'post_show.html', {'post':post})
+    categories = Category.objects.all()
+    data = {
+        'post': post,
+        'categories': categories
+    }
+    return render(request, 'post_show.html', data)
